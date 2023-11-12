ff02c6b498d4438505e5e1a96f4641b91bdb1150
kdestin <101366538+kdestin@users.noreply.github.com>
2023-11-11 21:11:01 -0500

    fix: Fix Pytest-IPython color scheme matching for IPython 8-8.17
        The monkeypatching solution used to match Pytest's colorscheme
        and the colorscheme IPython uses for tracebacks breaks for
        IPython 8.11 and up.
    
        This commit simplifies the monkeypatching logic to instead just
        replace the `get_style_for_name` function which one that returns
        the current pytes theme.
    
        For IPython 8.15 and up, we instead just use the newly added config
        value that controls traceback highlighting.
    
        This was manually validated to work for IPython 8.0.0 to 8.17.2, but
        we probably should have a way to check this in an automated way.

--- src/pytest_iovis/_subplugins/markup.py
+++ src/pytest_iovis/_subplugins/markup.py
@@ -11,10 +11,8 @@ class IPythonMarkupPlugin:
     """A user facing name that describes this plugin"""
 
     def __init__(self) -> None:
-        self.style_name: Optional[str] = os.getenv("PYTEST_THEME")
-        """The pygments style name to forward to IPython. None
-        means to use the default
-        """
+        self.style_name: str = os.getenv("PYTEST_THEME", "default")
+        """The pygments style name to forward to IPython. Defaults to 'default'"""
         self.should_output_color: bool = True
         """Whether to output color, or disable it. Defaults to True"""
 
@@ -29,50 +27,41 @@ class IPythonMarkupPlugin:
         if not self.should_output_color:
             return "--InteractiveShell.colors=NoColor"
 
-        # The latest version of IPython, 8.5.0 at time of writing, hard codes the
-        # styling for syntax highlighting when generating a traceback.
-        #
-        # The snippet below monkey-patches ipykernel to use the same pygments
-        # style as pytest for syntax highlighting
+        # Up until IPython 8.15, it was not possible to configure the colorscheme used for tracebacks.
+        # For IPython between 8.0.0 and 8.15.0, we monkeypatch IPython on startup to force it to use the right colors.
+        # For IPython above 8.15.0, we just set the appropriate config value.
         return f"""--IPKernelApp.exec_lines=
-    def set_traceback_highlighting_style(style_name):
-        from IPython import get_ipython
-        from pygments.formatters import TerminalFormatter, Terminal256Formatter
+    import IPython.core.ultratb
+    from IPython import version_info
+    def __set_traceback_highlighting_style():
+        '''Monkeypatch get_style_by_name in ultratb for IPython>=8,<8,15'''
         from pygments.styles import get_style_by_name
-        import stack_data
+        import functools
+        @functools.wraps(get_style_by_name)
+        def _(style: str) -> str:
+            return get_style_by_name({self.style_name!r})
 
-        def get_records(self, etb, number_of_lines_of_context: int, tb_offset: int):
-            assert etb is not None
-            context = number_of_lines_of_context - 1
-            after = context // 2
-            before = context - after
-            if self.has_colors:
-                style = get_style_by_name(style_name or "monokai")
-                style = stack_data.style_with_executing_node(style, "bg:ansiyellow")
-                formatter = Terminal256Formatter(style=style)
-            else:
-                formatter = None
-            options = stack_data.Options(
-                before=before,
-                after=after,
-                pygments_formatter=formatter,
-            )
-            return list(stack_data.FrameInfo.stack_data(etb, options=options))[tb_offset:]
+        IPython.core.ultratb.get_style_by_name = _
 
-        def bind(instance, func):
-            \"\"\"Binds func to instance\"\"\"
-            bound_method = func.__get__(instance, instance.__class__)
-            setattr(instance, func.__name__, bound_method)
-            return bound_method
+    def __set_ultra_tb_style():
+        '''Use the stopgap traceback config value introduced in IPython 8.15
 
-        bind(get_ipython().InteractiveTB, get_records)
+        .. see-also::
 
-    set_traceback_highlighting_style({self.style_name!r})
-    del set_traceback_highlighting_style"""
+            https://ipython.readthedocs.io/en/stable/whatsnew/version8.html#ipython-8-15
+            https://github.com/ipython/ipython/pull/14138
+        '''
+        from IPython import version_info
 
-    # This was fixed in IPython 8.15: https://ipython.readthedocs.io/en/stable/whatsnew/version8.html#ipython-8-15
-    # https://github.com/ipython/ipython/pull/14138
-    #
-    # Can replace the above with:
-    # from IPython.core.ultratb import VerboseTB
-    # VerboseTB._tb_highlight_style = "pastie"
+        if version_info >= (8, 15):
+            from IPython.core.ultratb import VerboseTB
+            VerboseTB._tb_highlight_style = {self.style_name!r}
+
+    if (8,) <= version_info and version_info < (8, 15):
+        __set_traceback_highlighting_style()
+    elif (8, 15) <= version_info:
+        __set_ultra_tb_style()
+
+    del __set_traceback_highlighting_style
+    del __set_ultra_tb_style
+    """
