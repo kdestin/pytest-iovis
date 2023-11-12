cf18556e5bfc4e5ddcaa1b8422690a2583b06356
kdestin <101366538+kdestin@users.noreply.github.com>
2023-11-11 22:14:18 -0500

    test: Add a test for papermill exception formatting

--- tests/test_papermill_runner.py
+++ tests/test_papermill_runner.py
@@ -1,6 +1,10 @@
+import json
 from pathlib import Path
+from typing import Callable, Optional
 
 import pytest
+
+from pytest_iovis import PathType
 
 
 class TestFixtures:
@@ -89,13 +93,61 @@ class TestFixtures:
 
 
 class TestRunner:
-    def test_default_test_function_runs_successfully(
-        self, dummy_notebook: Path, testdir: pytest.Testdir, monkeypatch: pytest.MonkeyPatch
-    ) -> None:
-        """Validate that the default test function runs a notebook successfully."""
+    @pytest.fixture()
+    def testdir(self, testdir: pytest.Testdir, monkeypatch: pytest.MonkeyPatch) -> pytest.Testdir:
         monkeypatch.setenv("IPYTHONDIR", str(Path(testdir.tmpdir, ".ipython")))
+        return testdir
+
+    @pytest.fixture()
+    def dummy_notebook_raise_exc(self, dummy_notebook_factory: Callable[[Optional[PathType]], Path]) -> Path:
+        nb_path = dummy_notebook_factory("assert_false")
+
+        with nb_path.open() as f:
+            nb = json.load(f)
+
+        nb["cells"] = [
+            {
+                "cell_type": "code",
+                "execution_count": None,
+                "id": "26ae4394",
+                "metadata": {},
+                "outputs": [],
+                "source": ["assert False"],
+            }
+        ]
+
+        with nb_path.open("w") as f:
+            json.dump(nb, f)
+
+        return nb_path
+
+    def test_default_test_function_runs_successfully(self, dummy_notebook: Path, testdir: pytest.Testdir) -> None:
+        """Validate that the default test function runs a notebook successfully."""
         res = testdir.runpytest(dummy_notebook, "-v")
 
         res.assert_outcomes(passed=1)
 
         res.stdout.fnmatch_lines("test_default_test_function_runs_successfully.ipynb::test_notebook_runs PASSED*")
+
+    def test_papermill_exception_formatting(
+        self, dummy_notebook_raise_exc: Path, testdir: pytest.Testdir, monkeypatch: pytest.MonkeyPatch
+    ) -> None:
+        """Validate that papermill exceptions fully replace pytest's exception tracebacks."""
+        monkeypatch.setenv("COLUMNS", "75")
+        res = testdir.runpytest(dummy_notebook_raise_exc)
+        res.assert_outcomes(failed=1)
+
+        res.stdout.fnmatch_lines(
+            [
+                "================================ FAILURES =================================",
+                "___________________________ test_notebook_runs ____________________________",
+                "---------------------------------------------------------------------------",
+                "AssertionError                            Traceback (most recent call last)",
+                "Cell In[1], line 1",
+                "----> 1 assert False",
+                "",
+                "AssertionError: ",
+                "-------------------------- Captured stderr call ---------------------------",
+            ],
+            consecutive=True,
+        )
